1b8803bee6017fd5a0515c8fc9c71ad1778c4e87
GeorgianIT <iulian.tudor2405@gmail.com>
2023-02-15 09:33:30 +0200

    add college projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,10 +13,10 @@ const Project = ({ data }) => {
         <img className="h-full w-full rounded-xl object-cover transition-transform duration-500  group-hover:scale-125" src={require(`../assets/${data.image}`)} />
       </div>
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-      <div className="absolute inset-0 flex translate-y-[70%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+      <div className="absolute inset-0 flex translate-y-[80%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
         <h1 className="font-dmserif text-3xl font-bold text-white">{data.title}</h1>
         {/* <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">I plan to build a travels project with places that I want to visit</p> */}
-        <a href={process.env.PUBLIC_URL + `${data.path}`} target='_blank' className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See Project</a>
+        <a href={process.env.PUBLIC_URL + `${data.path}`} target='_blank' download={`${data.title}.pdf`} className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See Project</a>
       </div>
     </motion.div>
   )
