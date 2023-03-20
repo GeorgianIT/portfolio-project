67a8a6c9f0fc3822713db298308d52e2d43547c6
GeorgianIT <iulian.tudor2405@gmail.com>
2023-03-20 09:26:16 +0200

    add target blank to error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -7,8 +7,8 @@ const ErrorPage = () => {
         <h1 className='font-bold text-lg'>Something went wrong!</h1>  
         <h2 className='text-lg'>Click here to see the project, or go back to portfolio</h2>
           <div className='mt-4'>
-            <button className="mr-2  bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"><a href='https://georgianit.github.io/portfolio-project/'>Go back</a></button>
-            <button className="ml-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"><a href='https://georgianit.github.io/coffee-shop/'>See Project</a></button>
+            <button className="mr-2  bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"><a href='https://georgianit.github.io/portfolio-project/' target='_blank'>Go back</a></button>
+            <button className="ml-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"><a href='https://georgianit.github.io/coffee-shop/' target='_blank'>See Project</a></button>
           </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
   //   element: <Navigate to='/error-page/'/>,
   // },
   {
-    path: "/coffee-shop/",
+    path: "/error-page/",
     element: <ErrorPage />,
   },
   {
